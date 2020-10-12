7f12d9cbf11ac2e8c60b9382d755cd4817ab72ed
Владислав Перескоков <pereskokow@gmail.com>
2020-10-12 16:37:57 +0300

    Handmade Hero Day 007 - Initializing DirectSound

--- win32_handmade.cpp
+++ win32_handmade.cpp
@@ -1,10 +1,24 @@
 #include <windows.h>
 #include <stdint.h>
+#include <xinput.h>
 
 typedef uint8_t uint8;
 typedef uint16_t uint16;
 typedef uint32_t uint32;
 typedef uint64_t uint64;
+typedef int8_t  int8;
+typedef int16_t int16;
+typedef int32_t int32;
+typedef int64_t int64;
+
+static int GlobalXOffset = 0;
+static int GlobalYOffset = 0;
+
+static bool isUpPressed = 0;
+static bool isDownPressed = 0;
+static bool isLeftPressed = 0;
+static bool isRightPressed = 0;
+
 
 struct win32_offscreen_buffer
 {
@@ -18,6 +32,41 @@ struct win32_offscreen_buffer
 
 static bool running;
 static win32_offscreen_buffer globalBackbuffer;
+
+// XInputGetState
+
+#define X_INPUT_GET_STATE(name) DWORD WINAPI name(DWORD dwUserIndex, XINPUT_STATE *pState)
+typedef X_INPUT_GET_STATE(x_input_get_state);
+X_INPUT_GET_STATE(XInputGetStateStub)
+{
+    return(0);
+}
+static x_input_get_state *XInputGetState_ = XInputGetStateStub;
+#define XInputGetState XInputGetState_
+
+// XInputSetState
+
+#define X_INPUT_SET_STATE(name) DWORD WINAPI name(DWORD dwUserIndex, XINPUT_VIBRATION *pVibration)
+typedef X_INPUT_SET_STATE(x_input_set_state);
+
+X_INPUT_SET_STATE(XInputSetStateStub)
+{
+    return(0);
+}
+static x_input_set_state *XInputSetState_ = XInputSetStateStub;
+#define XInputSetState XInputSetState_
+
+static void
+Win32LoadXInput(void)
+{
+    HMODULE xInputLibrary = LoadLibrary("xinput1_3.dll");
+
+    if (xInputLibrary)
+    {
+        XInputGetState = (x_input_get_state *)GetProcAddress(xInputLibrary, "XInputGetState");
+        XInputSetState = (x_input_set_state *)GetProcAddress(xInputLibrary, "XInputSetState");
+    }
+}
 
 static void
 RenderGradient(win32_offscreen_buffer *buffer, int xOffset, int yOffset)
@@ -149,6 +198,77 @@ MainWindowCallback(HWND window,
     {
         OutputDebugStringA("WM_ACTIVATEAPP\n");
     } break;
+    case WM_SYSKEYDOWN:
+    case WM_SYSKEYUP:
+    case WM_KEYDOWN:
+    case WM_KEYUP:
+    {
+        uint32 virtualKeyCode = wParam;
+        bool wasDown = ((lParam & (1 << 30)) != 0);
+        bool isDown ((lParam & (1 << 31)) == 0);
+
+        if (isDown != wasDown)
+        {
+            if (virtualKeyCode == 'W')
+            {
+                isUpPressed = isDown;
+            }
+            else if (virtualKeyCode == 'A')
+            {
+                isLeftPressed = isDown;
+            }
+            else if (virtualKeyCode == 'S')
+            {
+                isDownPressed = isDown;
+            }
+            else if (virtualKeyCode == 'D')
+            {
+                isRightPressed = isDown;
+            }
+            else if (virtualKeyCode == 'Q')
+            {
+            
+            }
+            else if (virtualKeyCode == 'E')
+            {
+            
+            }
+            else if (virtualKeyCode == VK_UP)
+            {
+            
+            }
+            else if (virtualKeyCode == VK_DOWN)
+            {
+            
+            }
+            else if (virtualKeyCode == VK_RIGHT)
+            {
+            
+            }
+            else if (virtualKeyCode == VK_LEFT)
+            {
+            
+            }
+            else if (virtualKeyCode == VK_ESCAPE)
+            {
+                OutputDebugStringA("Escape:");
+                if (isDown)
+                {
+                    OutputDebugStringA(" is down\n");
+                }
+                else if (wasDown)
+                {
+                    OutputDebugStringA(" was down\n");
+                }
+            }
+            else if (virtualKeyCode == VK_SPACE)
+            {
+            
+            }
+        }
+
+        
+    } break;
     default:
     {
         result = DefWindowProc(window, message, wParam, lParam);
@@ -164,6 +284,8 @@ int CALLBACK WinMain(
     LPSTR     commandLine,
     int       showCode)
 {
+    Win32LoadXInput();
+    
     WNDCLASSA windowClass = {};
     windowClass.style = CS_HREDRAW | CS_VREDRAW;
     windowClass.lpfnWndProc = MainWindowCallback;
@@ -208,7 +330,65 @@ int CALLBACK WinMain(
                     DispatchMessage(&message);
                 }
 
-                RenderGradient(&globalBackbuffer, xOffset++, 0);
+                // should pull this more frequently?
+                for (DWORD controllerIndex=0; controllerIndex < XUSER_MAX_COUNT; controllerIndex++)
+                {
+                    XINPUT_STATE controllerState;
+                    if (XInputGetState(controllerIndex, &controllerState) == ERROR_SUCCESS)
+                    {
+                        // controller is ok
+
+                        // look at controllerState.DwPacketNumber
+
+                        XINPUT_GAMEPAD *pad = &controllerState.Gamepad;
+
+                        bool dPadUp = (pad->wButtons & XINPUT_GAMEPAD_DPAD_UP);
+                        bool dPadDown = (pad->wButtons & XINPUT_GAMEPAD_DPAD_DOWN);
+                        bool dPadLeft = (pad->wButtons & XINPUT_GAMEPAD_DPAD_LEFT);
+                        bool dPadRight = (pad->wButtons & XINPUT_GAMEPAD_DPAD_RIGHT);
+                        bool dPadStart = (pad->wButtons & XINPUT_GAMEPAD_START);
+                        bool dPadBack = (pad->wButtons & XINPUT_GAMEPAD_BACK);
+                        bool dPadLeftShoulder = (pad->wButtons & XINPUT_GAMEPAD_LEFT_SHOULDER);
+                        bool dPadRightShoulder = (pad->wButtons & XINPUT_GAMEPAD_RIGHT_SHOULDER);
+                        bool dPadA = (pad->wButtons & XINPUT_GAMEPAD_A);
+                        bool dPadB = (pad->wButtons & XINPUT_GAMEPAD_B);
+                        bool dPadX = (pad->wButtons & XINPUT_GAMEPAD_X);
+                        bool dPadY = (pad->wButtons & XINPUT_GAMEPAD_Y);
+
+                        int16 stickX = pad->sThumbLX;
+                        int16 stickY = pad->sThumbLY;
+                    }
+                    else
+                    {
+                        // controller not available
+                    }
+                }
+
+                // send vibrations
+
+                // XINPUT_VIBRATION vibration;
+                // vibration.wLeftMotorSpeed = 69000;
+                // vibration.wRightMotorSpeed = 69000;
+                // XInputSetState(0, &vibration);
+
+                if (isLeftPressed)
+                {
+                    GlobalXOffset += 2;
+                }
+                if (isRightPressed)
+                {
+                    GlobalXOffset -= 2;
+                }
+                if (isUpPressed)
+                {
+                    GlobalYOffset += 2;
+                }
+                if (isDownPressed)
+                {
+                    GlobalYOffset -= 2;
+                }
+
+                RenderGradient(&globalBackbuffer, GlobalXOffset, GlobalYOffset);
 
                 HDC deviceContext = GetDC(windowHandle);
                 
