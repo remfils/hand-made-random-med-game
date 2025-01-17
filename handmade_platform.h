b48a11fc9e7da3a9b63ceb3c84fc241f9d5ed0c2
Vlad Pereskokov <remfils@ya.ru>
2025-01-18 01:22:29 +0300

    mend

--- handmade_platform.h
+++ handmade_platform.h
@@ -31,6 +31,20 @@ typedef int32_t int32;
 typedef int64_t int64;
 typedef int32 bool32;
 
+
+// shorten notation, TODO: remove rest
+typedef int32 b32;
+typedef int8_t  s8;
+typedef int16_t s16;
+typedef int32_t s32;
+typedef int64_t s64;
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+
+
 typedef float real32;
 typedef double real64;
 
